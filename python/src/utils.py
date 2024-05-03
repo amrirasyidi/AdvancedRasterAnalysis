327719428c32fc182cb34258bdf623f4c2a7488a
amrirasyidi <amri.geodesy@gmail.com>
2024-05-03 17:36:33 +0700

    remove metrics_score

--- python/src/utils.py
+++ python/src/utils.py
@@ -29,28 +29,4 @@ def reg_coef(
         fontsize=text_size
     )
     ax.set_axis_off()
-    
-def metrics_score(
-    model:BaseEstimator,
-    actual:pd.Series,
-    predicted:np.array
-    ):
-    try:
-        print(classification_report(actual, predicted, labels=model.classes_))
-    except:
-        print(classification_report(actual, [model.classes_[i] for i in predicted], labels=model.classes_))
 
-    cm = confusion_matrix(actual, predicted, labels=model.classes_)
-    
-    plt.figure(figsize=(8,5))
-    sns.heatmap(
-        cm,
-        annot=True,
-        fmt='g',
-        xticklabels=model.classes_,
-        yticklabels=model.classes_,
-        cmap='Blues',
-    )
-    plt.ylabel('Actual')
-    plt.xlabel('Predicted')
-    plt.show()
